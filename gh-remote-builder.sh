3aff04f1109be421e991f88ee6f35e06cc200d92
Talha Can Havadar <havadartalha@gmail.com>
2024-07-02 00:19:14 +0200

    add hardcoded build logic
    Signed-off-by: Talha Can Havadar <havadartalha@gmail.com>

--- gh-remote-builder.sh
+++ gh-remote-builder.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 
+gh workflow run amd64-build.yaml -f target_repo=talhaHavadar/governor.badgerd.nl -f target_ref=rust \
+    -f prebuild_command="curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && cargo install cargo-deb" \
+    -f build_command='cargo deb' -f target_files="target/debian"
+
+sleep 4s
+
+run_id=$(gh run list --workflow amd64-build.yaml | head -1 | awk -v FS='\t' '{ print $7}')
+
+gh run watch $run_id && gh run download $run_id && gh run delete $run_id
